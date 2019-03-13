9b41eff4f1402eb47f4fb5f23f38e896252f3bec
Nhi Tuyet <16110409@student.hcmute.edu.vn>
2019-03-13 22:48:01 +0700

    first commit

---
+++ NestedLinear/app/src/main/java/com/example/nestedlinear/Top_act.java
@@ -0,0 +1,13 @@
+package com.example.nestedlinear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Top_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_act);
+    }
+}
